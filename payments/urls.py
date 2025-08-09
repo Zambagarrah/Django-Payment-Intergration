607126dace0756bf10a537ac5c18ec760ca0cc43
Zambagarrah <zambagarrah@users.noreply.github.com>
2025-08-09 16:52:03 +0300

    chore: add /pay/mpesa/ route for Mpesa STK Push - Connects mpesa_checkout view to URL

--- payments/urls.py
+++ payments/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('success/', views.success, name='success'),
+    path("mpesa/", views.mpesa_checkout, name="mpesa_checkout"),
 ]
